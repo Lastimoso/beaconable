b1213059d5e13cdcc334c680ca291dd305f7e574
Lastimoso <gerardoraiden@gmail.com>
2019-08-24 11:24:32 -0300

    seting up and creating tests

--- test/beaconable_test.rb
+++ test/beaconable_test.rb
@@ -1,11 +1,34 @@
-require "test_helper"
+# frozen_string_literal: true
+
+require 'test_helper'
 
 class BeaconableTest < Minitest::Test
+  def setup
+    setup_db
+    @user = User.create(first_name: 'John', last_name: 'Rambo', email: 'rambo@gmail.com')
+    SideEffect.destroy_all
+  end
+
+  def teardown
+    teardown_db
+  end
+
   def test_that_it_has_a_version_number
     refute_nil ::Beaconable::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_object_creation_fire_beacon
+    User.create(first_name: 'Jack', last_name: 'Bauer', email: 'bauer@gmail.com') 
+    assert SideEffect.find_by(name: 'default').success?
+  end
+
+  def test_new_first_name_should_fire_specific_sideeffect
+    @user.update(first_name: 'Jack')
+    assert SideEffect.find_by(name: 'new_first_name').success?, 'New first name should fire specific side-effect'
+  end
+
+  def test_new_last_name_should_not_fire_specific_sideeffect
+    @user.update(last_name: 'Jack')
+    assert SideEffect.find_by(name: 'new_first_name').nil?, 'New Last Name should not fire new_first_name side-effect'
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,60 @@
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
-require "beaconable"
 
 require "minitest/autorun"
+require "minitest/spec"
+require "minitest/reporters"
+require "active_support"
+require "active_record"
+require "beaconable"
+
+Minitest::Reporters.use!
+
+ActiveRecord::Base.establish_connection(:adapter => "sqlite3", :database => ":memory:")
+
+class User < ActiveRecord::Base
+  include Beaconable
+end
+
+class SideEffect < ActiveRecord::Base
+end
+
+class UserBeacon < Beaconable::BaseBeacon
+
+  alias user object
+  alias user_was object_was
+
+  def call
+    SideEffect.create(name: 'default', success: true)
+    test_field_changed
+  end
+
+  private
+
+  def test_field_changed
+    SideEffect.create(name: 'new_first_name', success: true) if field_changed?(:first_name)
+  end
+end
+
+def setup_db
+  ActiveRecord::Migration.suppress_messages do
+    ActiveRecord::Schema.define(:version => 1) do
+      create_table :users do |t|
+        t.string :email, :limit => 255, :null => false
+        t.string :first_name, :limit => 100, :null => true
+        t.string :last_name, :limit => 100, :null => true
+      end
+      create_table :side_effects do |t|
+        t.string :name, limit: 255, null: false
+        t.boolean :success, default: false
+      end
+    end
+  end
+end
+
+def teardown_db
+  ActiveRecord::Migration.suppress_messages do
+    ActiveRecord::Base.connection.tables.each do |table|
+      ActiveRecord::Base.connection.drop_table(table)
+    end
+  end
+end
