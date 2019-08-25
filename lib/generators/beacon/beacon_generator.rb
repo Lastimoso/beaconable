1b7439e1454ff7b330dfd79a8f890ad94421a6fc
Lastimoso <gerardoraiden@gmail.com>
2019-08-25 11:35:47 -0300

    adding included Beaconable to model in generator

--- lib/generators/beacon/beacon_generator.rb
+++ lib/generators/beacon/beacon_generator.rb
@@ -5,7 +5,11 @@ require 'rails/generators/base'
 class BeaconGenerator < Rails::Generators::NamedBase
   source_root File.expand_path('templates', __dir__)
 
-  def create_serializer_file
+  def create_beacon_file
     template 'beacon.rb.tt', File.join('app', 'beacons', class_path, "#{file_name}_beacon.rb")
+  end
+
+  def insert_inclusion_into_model_file
+    inject_into_class "app/models/#{file_name}", class_name, "include Beaconable"
   end
 end
