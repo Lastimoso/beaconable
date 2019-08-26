b1fb4ec53375c38654ffa1a6eab9cc2bdb40755f
Lastimoso <gerardoraiden@gmail.com>
2019-08-26 10:59:29 -0300

    improving object_was_test

--- test/object_was_test.rb
+++ test/object_was_test.rb
@@ -6,18 +6,28 @@ class ObjectWasTest < Minitest::Test
   def setup
     setup_db
     @user = User.create(first_name: 'John', last_name: 'Rambo', email: 'rambo@gmail.com')
+    @user.first_name = 'Bruce'
+    @user_was = Beaconable::ObjectWas.new(@user).call
+    @user.save!
   end
 
   def teardown
     teardown_db
   end
 
-  def test_creates_object_was_successfully
-    @user.first_name = 'Bruce'
-    user_was = Beaconable::ObjectWas.new(@user).call
-    @user.save!
-    assert @user.first_name != user_was.first_name, 'first_name should be different'
+  def test_object_was_has_previous_field
+    assert @user.first_name != @user_was.first_name, 'first_name should be different'
     assert_equal 'Bruce', @user.first_name
-    assert_equal 'John', user_was.first_name
+    assert_equal 'John', @user_was.first_name
+  end
+
+  def test_object_was_keeps_other_fields
+    assert_equal @user.last_name, @user_was.last_name
+    assert_equal @user.email, @user_was.email 
+  end
+
+  def test_object_was_has_id_and_timestamp
+    assert_equal @user.id, @user_was.id
+    assert_equal @user.created_at, @user_was.created_at
   end
 end
